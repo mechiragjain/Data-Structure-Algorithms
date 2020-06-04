e073589627626d64777a46408019ce01f2f14ff8
Chirag Jain <me.chiragjain@gmail.com>
2020-06-04 23:14:26 +0530

    Solution Added

---
+++ Recursion/2 - Josephus Problem.cpp
@@ -0,0 +1,30 @@
+/*Given the total number of persons n and a number k which indicates that k-1 persons are skipped and kth person is killed in circle in a fixed direction.​
+The task is to choose the safe place in the circle so that when you perform these operations starting from 1st place in the circle, you are the last one remaining and survive.*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int josephus(int n, int k);
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n,k;
+		cin>>n>>k;
+		cout<<josephus(n,k)<<endl;
+	}
+	return 0;
+}
+
+int josephus(int n, int k)
+{
+   //Your code here
+    if(n==1)
+        return 1;
+    return (josephus(n-1,k)+k-1)%n+1;
+
+}
---
+++ Searching/4 - Leaders in an array.cpp
@@ -0,0 +1,44 @@
+/*Given an array A of positive integers. Your task is to find the leaders in the array*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> leaders(int arr[], int n){
+     vector<int> final;
+     int current=arr[n-1];
+     final.push_back(current);
+     for(int i=n-2;i>=0;i--){
+         if(current<=arr[i]){
+             final.push_back(arr[i]);
+             current=arr[i];
+         }
+     }
+     reverse(final.begin(),final.end());
+    return final;
+}
+
+int main()
+{
+   long long t;
+   cin >> t;
+   while (t--)
+   {
+       long long n;
+       cin >> n;
+        
+        int a[n];
+        
+        for(long long i =0;i<n;i++){
+            cin >> a[i];
+        }
+        
+        vector<int> v = leaders(a, n);
+        
+        for(auto it = v.begin();it!=v.end();it++){
+            cout << *it << " ";
+        }
+        
+        cout << endl;
+
+   }
+}
